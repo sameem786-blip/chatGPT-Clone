c96182e647147e406a36fb303e70103abb94f78b
Sameem Abbas <sameemabbas2002@outlook.com>
2023-12-21 19:46:19 +0500

    left tabs configured

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -4,6 +4,10 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import openaispiral from "../../public/openaispiral.png";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import StartIcon from "@mui/icons-material/Start";
 
 const Item = styled(Paper)(({ theme }) => ({
   borderRadius: "0px",
@@ -11,10 +15,11 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const Item1 = styled(Paper)(({ theme }) => ({
-  borderRadius: "10px", // Transition for smoother hover effect
+  borderRadius: "10px",
+  backgroundColor: "transparent",
+  color: "white", // Transition for smoother hover effect
   "&:hover": {
-    backgroundColor: "#202123", // Change this to your desired hover background color
-    color: "white",
+    backgroundColor: "#202123",
   },
 }));
 
@@ -27,18 +32,43 @@ const Dashboard = () => {
             className="Child-Grid-Item"
             style={{ backgroundColor: "black", color: "white" }}
           >
-            <Grid item xs={12} className="Item-Child-Grid-Container">
-              <Item1
-                className="Item-Child-Grid-Item"
-                elevation={0}
-              >
-                Hello
+            <Grid
+              item
+              xs={12}
+              direction="column"
+              justifyContent="space-between"
+              alignItems="center"
+              className="Item-Child-Grid-Container"
+            >
+              <Item1 className="Item-Child-Grid-Item" elevation={0}>
+                <div className="item-div">
+                  <div className="left-alignment">
+                    <div className="openai-logo-circle">
+                      <img
+                        className="left-openai-spiral"
+                        src={openaispiral}
+                        alt="openai"
+                      ></img>
+                    </div>
+                    <p className="left-item-text">New Chat</p>
+                  </div>
+                  <StartIcon className="icon-hover" />
+                </div>
               </Item1>
               <Item1
                 className="Item-Child-Grid-Item"
                 elevation={0}
+                style={{
+                  marginTop: "20px",
+                }}
               >
-                World
+                <div className="item-div-bottom">
+                  <div className="left-alignment">
+                    <AccountCircleIcon className="account-circle" />
+                    <p className="left-item-text">Sameem Abbas</p>
+                  </div>
+                  <MoreHorizIcon className="icon-hover" />
+                </div>
               </Item1>
             </Grid>
           </Item>
