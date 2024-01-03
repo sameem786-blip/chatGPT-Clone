60170972dcb474bbcdf5e996a5bcf15cd17d7c8d
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-03 19:20:10 +0500

    working on login func

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import LoginPage from "./pages/Login/LoginPage.jsx";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 
 function App() {
+  const isLoggedIn = false;
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/signin" element={<LoginPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={isLoggedIn ? <Dashboard /> : <LoginPage />}
+          />
           {/* Add more routes as needed */}
         </Routes>
       </Router>
