4283183dc4a4d9f76e5d2a444720bc4be63f1f9d
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-11 17:29:19 +0500

    check: correcting the check

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="569492616828-krv1lvkuqcqs2l562579v6094a5ri4sp.apps.googleusercontent.co">
+    <GoogleOAuthProvider clientId="569492616828-krv1lvkuqcqs2l562579v6094a5ri4sp.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
