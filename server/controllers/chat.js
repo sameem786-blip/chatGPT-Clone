1b145d310d66e3fd5e477fa905740ac3fe4a5941
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-11 16:46:38 +0500

    fix: mongodb transaction implemented on chained ops

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -6,12 +6,17 @@ const User = require("../schemas/Chat");
 const ChatGroup = require("../schemas/ChatGroup");
 const Chat = require("../schemas/Chat");
 
+const mongoose = require("mongoose");
 const axios = require("axios");
 
 exports.createNewChatGroup = async (req, res) => {
   try {
+    const response =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra et ultrices neque ornare aenean euismod elementum. Erat imperdiet sed euismod nisi porta lorem mollis aliquam. Euismod in pellentesque massa placerat duis ultricies lacus sed. Elit ut aliquam purus sit amet luctus venenatis lectus magna. Posuere ac ut consequat semper viverra nam. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. A erat nam at lectus urna duis convallis. Donec pretium vulputate sapien nec sagittis. Eget duis at tellus at urna condimentum. Ullamcorper malesuada proin libero nunc consequat interdum. Tempus iaculis urna id volutpat lacus laoreet non curabitur. Sed odio morbi quis commodo odio aenean sed. Facilisi morbi tempus iaculis urna id. Et ligula ullamcorper malesuada proin libero.";
     const userPrompt = req.body.prompt;
     const userId = req.userData.userId;
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
     const chatGroupObj = {
       name: userPrompt,
@@ -19,10 +24,9 @@ exports.createNewChatGroup = async (req, res) => {
       timeStamp: new Date(),
     };
 
-    const newChatGroup = await ChatGroup.create(chatGroupObj);
-
-    const response =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra et ultrices neque ornare aenean euismod elementum. Erat imperdiet sed euismod nisi porta lorem mollis aliquam. Euismod in pellentesque massa placerat duis ultricies lacus sed. Elit ut aliquam purus sit amet luctus venenatis lectus magna. Posuere ac ut consequat semper viverra nam. Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. A erat nam at lectus urna duis convallis. Donec pretium vulputate sapien nec sagittis. Eget duis at tellus at urna condimentum. Ullamcorper malesuada proin libero nunc consequat interdum. Tempus iaculis urna id volutpat lacus laoreet non curabitur. Sed odio morbi quis commodo odio aenean sed. Facilisi morbi tempus iaculis urna id. Et ligula ullamcorper malesuada proin libero.";
+    const newChatGroup = await ChatGroup.create([chatGroupObj], {
+      session: session,
+    });
 
     const chatObj = {
       prompt: userPrompt,
@@ -33,11 +37,14 @@ exports.createNewChatGroup = async (req, res) => {
       groupId: newChatGroup._id,
     };
 
-    const newChat = await Chat.create(chatObj);
+    const newChat = await Chat.create([chatObj], { session: session });
+
+    await session.commitTransaction();
+    session.endSession();
 
     const serverResponse = {
-      newChatGroup,
-      newChat: [newChat],
+      newChatGroup: newChatGroup[0],
+      newChat: newChat[0],
     };
 
     res.status(200).json(serverResponse);
